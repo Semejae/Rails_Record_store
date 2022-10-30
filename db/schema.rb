9de0db388328ddae85013dfc2c4e6bb8771a74b7
Anthony Gladden <a.gladden360@outlook.com>
2022-10-30 14:45:18 -0700

    added users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_09_204733) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_30_205149) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_09_204733) do
     t.integer "album_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_hash"
+    t.string "password_salt"
   end
 
   add_foreign_key "songs", "albums"
